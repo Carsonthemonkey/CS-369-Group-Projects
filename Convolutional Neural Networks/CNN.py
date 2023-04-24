f11470bda41d8f429a5e7a610c1dabadfc935786
Kat Berge <kat.n.berge@gmail.com>
2023-04-24 10:12:45 -0700

    Reads cvs in dataframe

--- Convolutional Neural Networks/CNN.py
+++ Convolutional Neural Networks/CNN.py
@@ -1,6 +1,9 @@
-#WHEEEEEEEEEEEEEEEEEEEEEEEE
+# WHEEEEEEEEEEEEEEEEEEEEEEEE
 # :)
+import pandas as pd
+
 # Read /home/labs/drake/cs369/answers.csv into a pandas dataframe (should have a length of 47703)
+df = pd.read_csv('/home/labs/drake/cs369/answers.csv')
 
 # Remove the two entries that don’t have images.
 
@@ -8,20 +11,20 @@
 
 # Set aside 20% of the data for testing.
 
-# Set aside 20% of the remaining data for validation. At this point, your training X values 
+# Set aside 20% of the remaining data for validation. At this point, your training X values
 # should have shape (30528, 53, 358) and your training y value should have shape (30528,).
 
 # Build a very simple network that has only a single output neuron with sigmoid activation.
 
-# Compile the model and fit it to the data. 10 epochs is probably good. When I did this, 
+# Compile the model and fit it to the data. 10 epochs is probably good. When I did this,
 # I got a validation accuracy of around 67%.
 
-# Now build a more complicated convolutional network. You might look at the example used on 
+# Now build a more complicated convolutional network. You might look at the example used on
 # Fashion MNIST from the book for inspiration.
 
-# Fit this more complicated model to the data. Plot the learning curve history. If you are able 
+# Fit this more complicated model to the data. Plot the learning curve history. If you are able
 # to get an accuracy over 80%, you’re good. If not, go back and tweak your model or hyperparameters.
 # I expect some teams will be able to do significantly better than my result!
 
-# Once you have your final model, train it one more time, being sure to save the learning curve 
+# Once you have your final model, train it one more time, being sure to save the learning curve
 # history plot and to test it on the test data.
